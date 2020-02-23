fc33e3d1f66c7a359415bb16737f539125772eeb
Hassan <850815h@gmail.com>
2020-02-23 23:53:01 +0100

    Klart projekt

--- src/com/company/SQLProgram.java
+++ src/com/company/SQLProgram.java
@@ -14,8 +14,6 @@ import java.util.ArrayList;
 public class SQLProgram {
     private Connection connection = null;
     private PreparedStatement preparedStatement;
-    private ResultSet resultSet;
-    private DatabaseMetaData db;
 
     private ArrayList<Room> rooms = new ArrayList<>();
     private ArrayList<Customer> customers = new ArrayList<>();
@@ -24,22 +22,10 @@ public class SQLProgram {
 
     private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private LocalDateTime localDateTime = LocalDateTime.now();
-    private String dateTime = localDateTime.format(dateTimeFormatter);
-    private String aaa = "POINT( positions.pos_lat, positions.pos_long );";
 
     public SQLProgram() {
         connect();
         refreshMethods();
-
-        ///////////////////////
-
-        /*for( Room s : rooms ){
-            System.out.println( s.getCity() + " " + s.getRoomSize() + " " + s.isAvailability() );
-        }*/
-
-        /*for( Customer customer : customers ){
-            System.out.println(customer.getName() + customer.getRoom());
-        }*/
     }
 
     public void refreshMethods(){
@@ -107,7 +93,7 @@ public class SQLProgram {
         }
     }
 
-    private void updateRoomAvailability(int roomId, int availability) {
+    public void updateRoomAvailability(int roomId, int availability) {
         String queries = "UPDATE rooms SET availability = ? WHERE id = ?;";
 
         try {
@@ -122,6 +108,8 @@ public class SQLProgram {
     }
 
     private void returnCustomers() {
+        ArrayList<Customer> tempCustomerList = new ArrayList<>();
+
         Statement stmt = null;
         String query = "SELECT * FROM customers";
         try {
@@ -133,15 +121,17 @@ public class SQLProgram {
                 String name = rs.getString("customer_name");
                 int room = rs.getInt("room");
 
-                customers.add( new Customer(id,name,room));
+                tempCustomerList.add( new Customer(id,name,room));
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        customers = tempCustomerList;
     }
 
     private void returnRooms() {
+        ArrayList<Room> tempRoomList = new ArrayList<>();
         Statement stmt = null;
         String query = "SELECT * FROM rooms";
         try {
@@ -162,55 +152,22 @@ public class SQLProgram {
                 boolean additionalServiceBoardHalf = rs.getBoolean("additional_services_board_half");
                 boolean additionalServiceBoardFull = rs.getBoolean("additional_services_board_full");
                 boolean additionalServiceExtraBed = rs.getBoolean("additional_services_extra_bed");
-                double pricePerNight = rs.getInt("price_per_night");
-                double rating = rs.getInt("rating");
+                double pricePerNight = rs.getDouble("price_per_night");
+                double rating = rs.getDouble("rating");
+                double distanceToCity = rs.getDouble("distance_to_city");
+                double distanceToBeach = rs.getDouble("distance_to_beach");
                 boolean availability = rs.getBoolean("availability");
 
-                rooms.add(new Room(id, selectCityName(city), roomSize, bookingStart, bookingEnd, maxAmountOfCustomers, facilitiesRestaurant, facilitiesPool,
+                tempRoomList.add(new Room(id, selectCityName(city), roomSize, bookingStart, bookingEnd, maxAmountOfCustomers, facilitiesRestaurant, facilitiesPool,
                         facilitiesEveningEntertainment, facilitiesChildrenClub, additionalServiceBoardHalf, additionalServiceBoardFull, additionalServiceExtraBed,
-                        pricePerNight, rating, availability));
+                        pricePerNight, rating, distanceToCity, distanceToBeach, availability));
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        rooms = tempRoomList;
     }
-
-    /*public ObservableList<Room> returnRoomsAvailableRestaurant() {
-        ObservableList<Room> roomsAvailableRestaurant = FXCollections.observableArrayList();
-        Statement stmt = null;
-        String query = "SELECT * FROM rooms WHERE facilities_restaurant = 1";
-        try {
-            stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-                int city = rs.getInt("city");
-                int roomSize = rs.getInt("room_size");
-                Date bookingStart = rs.getDate("booking_start");
-                Date bookingEnd = rs.getDate("booking_end");
-                int maxAmountOfCustomers = rs.getInt("max_amount_of_customer");
-                boolean facilitiesRestaurant = rs.getBoolean("facilities_restaurant");
-                boolean facilitiesPool = rs.getBoolean("facilities_pool");
-                boolean facilitiesEveningEntertainment = rs.getBoolean("facilities_evening_entertainment");
-                boolean facilitiesChildrenClub = rs.getBoolean("facilities_children_club");
-                boolean additionalServiceBoardHalf = rs.getBoolean("additional_services_board_half");
-                boolean additionalServiceBoardFull = rs.getBoolean("additional_services_board_full");
-                boolean additionalServiceExtraBed = rs.getBoolean("additional_services_extra_bed");
-                double pricePerNight = rs.getInt("price_per_night");
-                double rating = rs.getInt("rating");
-                boolean availability = rs.getBoolean("availability");
-
-                roomsAvailableRestaurant.add(new Room(selectCityName(city), roomSize, bookingStart, bookingEnd, maxAmountOfCustomers, facilitiesRestaurant, facilitiesPool,
-                        facilitiesEveningEntertainment, facilitiesChildrenClub, additionalServiceBoardHalf, additionalServiceBoardFull, additionalServiceExtraBed,
-                        pricePerNight, rating, availability));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return roomsAvailableRestaurant;
-    }*/
 
     private String selectCityName(int cityID) {
         Statement stmt = null;
@@ -262,107 +219,6 @@ public class SQLProgram {
         return userName + " added!";
     }
 
-    public String deleteCustomer(String userName) {
-        for (String name : returnCustomersNames()) {
-            if (!name.equalsIgnoreCase(userName)) {
-                return "Name already exists!";
-            }
-        }
-
-        String query = "DELETE FROM customers ("
-                + "customer_name )" +
-                "VALUES (?)";
-
-        try {
-            preparedStatement = connection.prepareStatement(query);
-
-            preparedStatement.setString(1, userName);
-            preparedStatement.executeUpdate();
-            preparedStatement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return userName + " added.";
-    }
-
-    private void insertIntoCustomers(String name) {
-        String query = "REPLACE customers ("
-                + "id, customer_name )" +
-                "VALUES (?, ?)";
-
-        try {
-            preparedStatement = connection.prepareStatement(query);
-
-            preparedStatement.setInt(1, 20);
-            preparedStatement.setString(2, name);
-            preparedStatement.executeUpdate();
-            preparedStatement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void updatePositions(int posCarPickupDropoff, double positionLat, double positionLong) {
-        String[] queries = {
-                "UPDATE cars INNER JOIN positions SET position_car = POINT( ( ? ), ( ? ) );",
-                "UPDATE cars INNER JOIN positions SET position_customer_pickup = POINT( ( ? ), ( ? ) );",
-                "UPDATE cars INNER JOIN positions SET position_customer_dropoff = POINT( ( ? ), ( ? ) );"};
-
-        try {
-            preparedStatement = connection.prepareStatement(queries[posCarPickupDropoff]);
-            preparedStatement.setDouble(1, positionLat);
-            preparedStatement.setDouble(2, positionLong);
-            preparedStatement.executeUpdate();
-            preparedStatement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void replaceIntoCars(int id, String model, double pricePerKm, int owner, int customer, String
-            bookingStart, String bookingEnd,
-                                 double positionCarLat, double positionCarLong, double positionCustomerPickUpLat,
-                                 double positionCustomerPickUpLong,
-                                 double positionCustomerDropOffLat, double positionCustomerDropOffLong) {
-
-        String queryReplaceInto = "REPLACE cars( id, model, price_per_km, owner, customer, booking_start, booking_end, position_car, position_customer_pickup, position_customer_dropoff, total_distance_in_km )" +
-                "VALUES( ?, ?, ?, ?, ?, ?, ?, POINT(0,0), POINT(0,0), POINT(0,0), ? );";
-
-        try {
-
-            preparedStatement = connection.prepareStatement(queryReplaceInto);
-            preparedStatement.setInt(1, id);
-            preparedStatement.setString(2, model);
-            preparedStatement.setDouble(3, pricePerKm);
-            preparedStatement.setInt(4, owner);
-            preparedStatement.setInt(5, customer);
-            preparedStatement.setString(6, bookingStart);
-            preparedStatement.setString(7, bookingEnd);
-            preparedStatement.setDouble(8, 10.1);
-            preparedStatement.executeQuery();
-
-            updatePositions(0, positionCarLat, positionCarLong);
-            updatePositions(1, positionCustomerPickUpLat, positionCustomerPickUpLong);
-            updatePositions(2, positionCustomerDropOffLat, positionCustomerDropOffLong);
-
-            preparedStatement.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void searchByFirstName(String name) {
-        try {
-            preparedStatement = connection.prepareStatement("SELECT * FROM cars WHERE model LIKE ?");
-            preparedStatement.setString(1, name); //1an är för vilken %?% det gället från förra raden ("SELECT * FROM persons WHERE name LIKE %?%").
-            resultSet = preparedStatement.executeQuery();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     private ArrayList<String> returnCustomersNames() {
         ArrayList<String> names = new ArrayList<>();
         Statement stmt = null;
@@ -392,21 +248,14 @@ public class SQLProgram {
         }
     }
 
-    public ArrayList<String> getCustomerNames() {
-        return customerNames;
-    }
-
     public ArrayList<Customer> getCustomers() {
+        returnCustomers();
         return customers;
     }
 
     public ArrayList<Room> getRooms() {
+        returnRooms();
         return rooms;
     }
-
-    public ArrayList<String> getCityNames() {
-        return cityNames;
-    }
-    //public ObservableList<Room> getRoomsAvailableRestaurant(){ return roomsAvailableRestaurant; }
 
 }
